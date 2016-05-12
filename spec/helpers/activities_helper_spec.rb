a151f7eaaa20ed1f181905d4941f295ecdd3eb4c
Wouter Kooyman van Guldener <wouter@wick-it.nl>
2016-05-12 19:22:47 +0200

    Fixed rubocop offenses.

--- spec/helpers/activities_helper_spec.rb
+++ spec/helpers/activities_helper_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ActivitiesHelper, type: :helper do
-
   describe '#ActivitiesHelper' do
-
     describe '#engine?' do
       it 'returns whether or not the activity is an engine' do
-        allow(controller).to receive(:params).and_return({:controller => "bepstore/activities"})
+        allow(controller).to receive(:params).and_return(controller: 'bepstore/activities')
         expect(helper.engine?).to eq(false)
       end
 
       it 'returns whether or not the activity is an engine' do
-        allow(controller).to receive(:params).and_return({:controller => "bepstore/goals"})
+        allow(controller).to receive(:params).and_return(controller: 'bepstore/goals')
         expect(helper.engine?).to eq(true)
       end
     end
@@ -20,13 +18,13 @@ RSpec.describe ActivitiesHelper, type: :helper do
       # let!(:activity.present) {true}
 
       it 'returns whether or not the activity is an engine' do
-        allow(controller).to receive(:params).and_return({:controller => "bepstore/goals"})
-        expect(helper.root).to eq("goals")
+        allow(controller).to receive(:params).and_return(controller: 'bepstore/goals')
+        expect(helper.root).to eq('goals')
       end
 
       it 'returns whether or not the activity is an engine' do
-        allow(controller).to receive(:params).and_return({:controller => "bepstore/activities"})
-        expect(helper.root).to eq("activities")
+        allow(controller).to receive(:params).and_return(controller: 'bepstore/activities')
+        expect(helper.root).to eq('activities')
       end
     end
   end
--- spec/helpers/api_helper_spec.rb
+++ spec/helpers/api_helper_spec.rb
@@ -1,26 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe ApiHelper, type: :helper do
-
   describe '#ApiHelper' do
-
     describe '#resource_name' do
-
       it 'returns whether or not the activity is an engine' do
-        allow(controller).to receive(:params).and_return({:controller => "bepstore/activities"})
-        expect(resource_name).to eq("activity")
+        allow(controller).to receive(:params).and_return(controller: 'bepstore/activities')
+        expect(resource_name).to eq('activity')
       end
-
     end
 
     describe '#resource_class' do
-
       it 'returns whether or not the activity is an engine' do
-        allow(controller).to receive(:params).and_return({:controller => "bepstore/activities_controllers"})
-        stub_const("ActivitiesController", 5)
+        allow(controller).to receive(:params).and_return(controller: 'bepstore/activities_controllers')
+        stub_const('ActivitiesController', 5)
         expect(resource_class).to eq(5)
       end
-      
     end
   end
 end
