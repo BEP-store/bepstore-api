f0c1fb5eae7693d55425613fa6c4aad83d5ab9b4
Wouter Kooyman van Guldener <wouter@wick-it.nl>
2016-05-13 12:59:33 +0200

    Accidentally added a file too much.

--- spec/controllers/v1/activities_controller_spec.rb
+++ spec/controllers/v1/activities_controller_spec.rb
@@ -7,13 +7,6 @@ RSpec.describe V1::ActivitiesController, type: :controller do
         get :index
       end
     end
-
-    describe 'should return the activities' do
-        before { action.call }
-
-        it { expect(JSON.parse(response.body)['activities'][0]['id']).to eq(activity2.id.to_s) }
-        it { expect(JSON.parse(response.body)['activities'][1]['id']).to eq(activity.id.to_s) }
-      end
   end
 
   describe 'GET #find' do
