33c7c523f1f0b5d28ee36856c9afe136b43460a6
Wouter Kooyman van Guldener <wouter@wick-it.nl>
2016-05-19 12:35:53 +0200

    Deleted unnecessary basic auth helper.

--- spec/support/http_basic_auth_helper.rb
+++
@@ -1,5 +0,0 @@
-module HttpBasicAuthHelper
-  def http_basic_auth_login(email, password)
-    request.env['AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(email, password)
-  end
-end
