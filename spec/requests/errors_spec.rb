590fc613244b22c66f711b2c390408086dd6e274
Wouter Kooyman van Guldener <wouter@wick-it.nl>
2016-06-17 15:08:35 +0200

    Made test descriptions more descriptive.

--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Errors', type: :request do
-  describe 'GET /' do
-    it 'works! (now write some real specs)' do
+  describe 'GET /{errorcode}' do
+    it 'should return a 404' do
       get '/404'
       expect(response).to have_http_status(404)
     end
 
-    it 'works! (now write some real specs)' do
+    it 'should return a 422' do
       get '/422'
       expect(response).to have_http_status(422)
     end
 
-    it 'works! (now write some real specs)' do
+    it 'should return a 500' do
       get '/500'
       expect(response).to have_http_status(500)
     end
--- spec/routing/errors_routing_spec.rb
+++ spec/routing/errors_routing_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ErrorsController, type: :routing do
-  describe 'routing' do
-   it 'routes to #show' do
+  describe 'error routing' do
+   it 'routes to #show on 404' do
       expect(get: '/404').to route_to('errors#show', code: '404')
    end
 
-   it 'routes to #show' do
+   it 'routes to #show  on 422' do
      expect(get: '/422').to route_to('errors#show', code: '422')
    end
 
-   it 'routes to #show' do
+   it 'routes to #show on 500' do
      expect(get: '/500').to route_to('errors#show', code: '500')
    end
   end
